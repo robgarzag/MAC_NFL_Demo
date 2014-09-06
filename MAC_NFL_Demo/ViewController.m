1a968665255cc76885a50d86cc7f6cb495960695
robgarzag <rob_garzag@hotmail.com>
2014-09-06 19:13:58 -0400

    Initial Commit

---
+++ MAC_NFL_Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MAC_NFL_Demo
+//
+//  Created by MobileAppsCompany on 9/6/14.
+//  Copyright (c) 2014 RGG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MAC_NFL_Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MAC_NFL_Demo
+//
+//  Created by MobileAppsCompany on 9/6/14.
+//  Copyright (c) 2014 RGG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
